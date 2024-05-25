7a89771dd3419d856494de49bc937584dac5cbd8
Abagels96 <138115235+Abagels96@users.noreply.github.com>
2024-05-24 19:16:54 -0500

    Update Example.java

--- Example.java
+++ Example.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Example {
 public static void main(String[] args) {
 MethodTesting method= new MethodTesting();
-Integer flarf= method.getNum();
-System.out.println(flarf);
+Integer copy= method.getNum();
+System.out.println(copy);
 for(int i=0; i<=5; i++){
 	 System.out.println(method.newMethod());
 
-	if (flarf>=50 && flarf<=300) {
+	if (copy>=50 && copy<=300) {
 	i=5;
 		
 	}
